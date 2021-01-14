8851af10cf02e1a4adc8fa8994a1aef1c15bfde3
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-01-14 11:46:13 +0100

    style: prettify .cjs files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,40 +1,40 @@
 module.exports = {
-	printWidth: 80,
-	endOfLine: 'lf',
-	overrides: [
-		{
-			files: '*.{ts,tsx}',
-			options: {
-				parser: 'typescript',
-				singleQuote: true,
-				trailingComma: 'all',
-				useTabs: true,
-				semi: false,
-			},
-		},
-		{
-			files: '*.js',
-			options: {
-				parser: 'babel',
-				singleQuote: true,
-				trailingComma: 'all',
-				useTabs: true,
-				semi: false,
-			},
-		},
-		{
-			files: '*.md',
-			options: {
-				parser: 'markdown',
-				proseWrap: 'always',
-			},
-		},
-		{
-			files: '*.{yml,yaml}',
-			options: {
-				parser: 'yaml',
-				proseWrap: 'always',
-			},
-		},
-	],
-}
+  printWidth: 80,
+  endOfLine: "lf",
+  overrides: [
+    {
+      files: "*.{ts,tsx}",
+      options: {
+        parser: "typescript",
+        singleQuote: true,
+        trailingComma: "all",
+        useTabs: true,
+        semi: false,
+      },
+    },
+    {
+      files: "*.js",
+      options: {
+        parser: "babel",
+        singleQuote: true,
+        trailingComma: "all",
+        useTabs: true,
+        semi: false,
+      },
+    },
+    {
+      files: "*.md",
+      options: {
+        parser: "markdown",
+        proseWrap: "always",
+      },
+    },
+    {
+      files: "*.{yml,yaml}",
+      options: {
+        parser: "yaml",
+        proseWrap: "always",
+      },
+    },
+  ],
+};
